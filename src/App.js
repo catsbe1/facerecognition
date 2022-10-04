ef3d66cf4ae1a8d889a34f35fcc0f450c2b658b3
catsbe1 <anaribeiro199814@gmail.com>
2022-10-04 03:09:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://safe-anchorage-08994.herokuapp.com/:3001/image", {
+          fetch("https://safe-anchorage-08994.herokuapp.com/image", {
             method: "put",
             headers: { "Content-type": "application/json" },
             body: JSON.stringify({
